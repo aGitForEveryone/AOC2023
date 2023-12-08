b7a37325ae757aebc545bb59e950b69ddf4528e0
Tobias Van Damme <tobias.damme@skf.com>
2023-12-08 09:54:43 +0100

    Solved part a working on part b day 8

--- day08/day8.py
+++ day08/day8.py
@@ -1,4 +1,5 @@
 import re
+from pathlib import Path
 
 from aocd import get_data, submit
 import numpy as np
@@ -15,31 +16,85 @@ def parse_data(load_test_data: bool = False):
                             directory
     """
     if load_test_data:
-        with open("input8.1", "r") as f:
+        with open(Path(__file__).parent / "input8.3", "r") as f:
             # For loading example or test data
             data = f.read()
     else:
         data = get_data(day=8, year=2023)
-    # lines = data.splitlines()
+
+    steps = data.splitlines()[0]
+    instructions = get_instructions(data)
+
     # grid = np.array(helper_functions.digits_to_int(data.splitlines()))
     # numbers = [int(x) for x in re.findall("(-?\d+)", data)]
-    return data
+    return steps, instructions
+
+
+def get_instructions(data: str) -> dict[str, tuple[str, str]]:
+    pattern = r"([A-Z1-9]{3}) = \(([A-Z1-9]{3}), ([A-Z1-9]{3})\)"
+    instructions = {}
+    for match in re.findall(pattern, data):
+        instructions[match[0]] = match[1:]
+
+    return instructions
 
 
 def part1(data):
     """Advent of code 2023 day 8 - Part 1"""
-    answer = 0
+    steps_taken = 0
+    cur_position = "AAA"
+    target_position = "ZZZ"
+    step_to_idx = {
+        "L": 0,
+        "R": 1,
+    }
+    steps = helper_functions.yield_next_from_iterator(data[0])
+    while cur_position != target_position:
+        next_step = next(steps)
+        cur_position = data[1][cur_position][step_to_idx[next_step]]
+        steps_taken += 1
 
-    print(f"Solution day 8, part 1: {answer}")
-    return answer
+    print(f"Solution day 8, part 1: {steps_taken}")
+    return steps_taken
+
+
+def get_starting_nodes(data: dict[str, tuple[str, str]]) -> list[str]:
+    nodes = []
+    for node in data.keys():
+        if node[-1] == "A":
+            nodes.append(node)
+    return nodes
+
+
+def reached_destination(current_positions: list[str]) -> bool:
+    return all([position[-1] == "Z" for position in current_positions])
 
 
 def part2(data):
     """Advent of code 2023 day 8 - Part 2"""
-    answer = 0
+    current_positions = {node: [node] for node in get_starting_nodes(data[1])}
+    steps_taken = 0
+    step_to_idx = {
+        "L": 0,
+        "R": 1,
+    }
+    for next_step in data[0]:
+        for position, places_visited in current_positions.items():
+            next_position = data[1][places_visited[-1]][step_to_idx[next_step]]
+            current_positions[position].append(next_position)
 
-    print(f"Solution day 8, part 2: {answer}")
-    return answer
+    print(current_positions)
+    # steps = helper_functions.yield_next_from_iterator(data[0])
+    # while not reached_destination(current_positions):
+    #     next_step = next(steps)
+    #     new_positions = []
+    #     for position in current_positions:
+    #         new_positions.append(data[1][position][step_to_idx[next_step]])
+    #     current_positions = new_positions
+    #     steps_taken += 1
+
+    print(f"Solution day 8, part 2: {steps_taken}")
+    return steps_taken
 
 
 def main(parts: str, should_submit: bool = False, load_test_data: bool = False) -> None:
@@ -72,6 +127,6 @@ if __name__ == "__main__":
     # test_data = True
     submit_answer = False
     # submit_answer = True
-    main("a", should_submit=submit_answer, load_test_data=test_data)
-    # main("b", should_submit=submit_answer, load_test_data=test_data)
+    # main("a", should_submit=submit_answer, load_test_data=test_data)
+    main("b", should_submit=submit_answer, load_test_data=test_data)
     # main("ab", should_submit=submit_answer, load_test_data=test_data)
--- day08/test_day8.py
+++ day08/test_day8.py
@@ -38,7 +38,24 @@ class TestDay8(unittest.TestCase):
         """Test day8.part2"""
         result = day8.part2(TEST_DATA)
 
+    def test_loops(self):
+        data = day8.parse_data(load_test_data=False)
+        current_position = "AAA"
+        step_to_idx = {
+            "L": 0,
+            "R": 1,
+        }
+        loop_positions = [current_position]
+        while len(loop_positions) == len(set(loop_positions)):
+            # intermediate_positions = []
+            for next_step in data[0]:
+                current_position = data[1][current_position][step_to_idx[next_step]]
+                # intermediate_positions.append(current_position)
+            # print(intermediate_positions)
+            loop_positions.append(current_position)
+
+        print(loop_positions)
+
 
 if __name__ == "__main__":
     unittest.main(module="test_day8")
-
