62ab8fc2dc9411f707b220e8a18392aa40dcef25
aGitForEveryone <f1competitie3@gmail.com>
2023-12-13 15:40:37 +0100

    added day 13

---
+++ day13/test_day13.py
@@ -0,0 +1,44 @@
+# Unit testing
+"""
+@author: Tobias Van Damme
+"""
+
+import unittest
+import json
+from pathlib import Path
+
+import numpy as np
+
+import helper_functions
+from . import day13
+
+TEST_FOLDER = Path(__file__).parent
+
+with open(TEST_FOLDER / "input13.1", "r") as f:
+    # For loading example or test data
+    TEST_DATA = f.read()
+
+
+class TestDay13(unittest.TestCase):
+    """Test class to test functions in day13.day13"""
+
+    def setUp(self):
+        """Set up the tests"""
+        pass
+
+    def tearDown(self):
+        """Clean up"""
+        pass
+
+    def test_part1(self):
+        """Test day13.part1"""
+        result = day13.part1(TEST_DATA)
+
+    def test_part2(self):
+        """Test day13.part2"""
+        result = day13.part2(TEST_DATA)
+
+
+if __name__ == "__main__":
+    unittest.main(module="test_day13")
+
