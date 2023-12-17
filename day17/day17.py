8a5f592a7468dcc81ee49f74d157863234f89d36
Tobias Van Damme <tobias.van.damme@skf.com>
2023-12-17 19:30:31 +0100

    Attempt 2 for part 1

--- day17/day17.py
+++ day17/day17.py
@@ -28,7 +28,7 @@ def parse_data(load_test_data: bool = False):
     return grid
 
 
-def find_valid_neighbors(
+def find_valid_directions(
     cur_pos: Coordinate,
     moving_direction: Coordinate,
     times_moving_straight: int,
@@ -36,7 +36,7 @@ def find_valid_neighbors(
 ):
     """Finds valid neighbors given the current position and moving direction. We cannot turn back, and can at most move
     straight 3 times in a row before we have to turn left or right."""
-    valid_directions = [
+    return [
         direction
         for direction in [
             Direction.LEFT.value,
@@ -50,31 +50,64 @@ def find_valid_neighbors(
         )
     ]
 
-    return [
-        cur_pos + direction
-        for direction in valid_directions
-        if Coordinate(0, 0) <= cur_pos + direction < Coordinate(grid_size)
-    ]
+    # return [
+    #     cur_pos + direction
+    #     for direction in valid_directions
+    #     if Coordinate(0, 0) <= cur_pos + direction < Coordinate(grid_size)
+    # ]
+
+
+def sort_possible_paths(
+    possible_paths: list[tuple[Coordinate, Coordinate, int, int]]
+) -> list[tuple[Coordinate, Coordinate, int, int]]:
+    """Sorts the possible paths in order of path value"""
+    return sorted(possible_paths, key=lambda x: x[3])
 
 
 def part1(data):
     """Advent of code 2023 day 17 - Part 1"""
     start = Coordinate(0, 0)
+    end = Coordinate(len(data) - 1, len(data[0]) - 1)
     grid_size = Coordinate(len(data), len(data[0]))
     # When starting at (0, 0), after 1 iteration there will be 2 possible paths
     # We manually add these to the list of possible paths because the moving direction
     # is undefined at the start.
     # We define a path by the tuple: (current position, moving direction, times moving straight, path_value)
-    possible_paths = [
-        (Coordinate(1, 0), Direction.DOWN.value, 1, data[1][0]),  # Moving down
-        ((0, 1), (0, 1), 1, data[0][1]),  # Moving right
-    ]
-    next_possible_paths = []
-    for path in possible_paths:
-        valid_neighbors = find_valid_neighbors(path[0], path[1], path[2], grid_size)
-    print(possible_paths)
+    possible_paths = sort_possible_paths(
+        [
+            (Direction.DOWN.value, Direction.DOWN.value, 1, data[1][0] + data[0][0]),
+            (
+                Direction.RIGHT.value,
+                Direction.RIGHT.value,
+                1,
+                data[0][1] + data[0][0],
+            ),
+        ]
+    )
+    has_visited = {start, Direction.DOWN.value, Direction.RIGHT.value}
+    # Now we advance the lowest value path only. We pop the first path, advance it,
+    # re-evaluate the path value, and add it again to the list of possible paths, and
+    # sort the list again. Rinse and repeat until the lowest value path is at the end.
+    while possible_paths[0][0] != end:
+        path = possible_paths.pop(0)
+        valid_directions = find_valid_directions(path[0], path[1], path[2], grid_size)
+        new_paths = []
+        for direction in valid_directions:
+            new_pos = path[0] + direction
+            if start <= new_pos <= end and new_pos not in has_visited:
+                new_paths += [
+                    (
+                        new_pos,
+                        direction,
+                        path[2] + 1 if path[1] == direction else 1,
+                        path[3] + data[new_pos[0]][new_pos[1]],
+                    )
+                ]
+                has_visited.add(new_pos)
+        possible_paths = sort_possible_paths(new_paths + possible_paths)
 
-    answer = 0
+    print(possible_paths)
+    answer = possible_paths[0][3]
 
     print(f"Solution day 17, part 1: {answer}")
     return answer
@@ -114,8 +147,8 @@ def main(parts: str, should_submit: bool = False, load_test_data: bool = False) 
 
 
 if __name__ == "__main__":
-    test_data = False
-    # test_data = True
+    # test_data = False
+    test_data = True
     submit_answer = False
     # submit_answer = True
     main("a", should_submit=submit_answer, load_test_data=test_data)
